4dc11f6b11399fb38e53c92c34177ee75c5a4513
NullBrunk <anasi10@proton.me>
2025-12-28 14:01:49 +0100

    Add setters for endpoint, param, suspicion

--- core/context.py
+++ core/context.py
@@ -13,6 +13,18 @@ class Context:
         self.suspicions = set()
         self.vulns = []
 
+    def add_endpoint(self, path, method="GET"):
+        self.endpoints.setdefault(path, set()).add(method)
+
+    def add_param(self, path, param):
+        self.params.setdefault(path, set()).add(param)
+
+    def add_suspicion(self, vuln_type):
+        self.suspicions.add(vuln_type)
+
+    def add_vuln(self, vuln):
+        self.vulns.append(vuln)
+
     def __iter__(self):
         yield "endpoints", {k: list(v) for k, v in self.endpoints.items()}
         yield "params", {k: list(v) for k, v in self.params.items()}
